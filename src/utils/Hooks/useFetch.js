7c647c61c918705800b67bdf01cae035320e9e88
paulinegc <francasix@outlook.com>
2021-05-27 14:47:03 +0200

    utils

---
+++ src/utils/Hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch({ api, method, url, data = null, config = null }) {
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                api[method](url, JSON.parse(config), JSON.parse(data))
+                    .then((res) => {
+                        setResponse(res.data);
+                    })
+                    .finally(() => {
+                        setIsLoading(false);
+                    });
+            } catch (err) {
+                setError(err);
+            }
+        };
+
+        fetchData();
+    }, [api, method, url, data, config]);
+
+    return { response, error, isLoading };
+}
