9424b09af4f116a09f9a9436a8d1a5c21ff4156e
jerem1508 <jeremy.peglion@gmail.com>
2021-06-01 10:30:36 +0200

    1

--- src/pages/BaroSante/SantePublications/index.js
+++ src/pages/BaroSante/SantePublications/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import BarsChart from '../../../components/charts/bars-chart';
+import LinesChart from '../../../components/charts/lines-chart';
 
-function EssaisCliniques() {
+function Publications() {
   return (
     <div>
       <h1>Publications</h1>
+      <div>
+        <div>
+          Quelle est la dynamique d’ouverture de la santé en France ?
+          <span>i</span>
+        </div>
+        <div>Text intro du graph</div>
+        <BarsChart idChart="chartPublicationsGeneral_1" />
+        <LinesChart idChart="chartPublicationsGeneral_2" />
+      </div>
     </div>
   );
 }
 
-export default EssaisCliniques;
+export default Publications;
