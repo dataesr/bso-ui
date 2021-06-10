10cf920f13a44b34ba5f82b65bcd4b452ba45891
folland87 <frederic@olland.me>
2021-06-10 17:37:03 +0200

    fix lint errors

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,5 +1,4 @@
-export const ES_API_URL =
-  'https://cluster.elasticsearch.dataesr.ovh/bso-publications/_search';
+export const ES_API_URL = 'https://cluster.elasticsearch.dataesr.ovh/bso-publications/_search';
 
 export const HEADERS = {
   headers: {
