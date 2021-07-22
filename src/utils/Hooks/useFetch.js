09f29934f9eb5fcacaf308ce7c121c8ddf53d933
paulinegc <francasix@outlook.com>
2021-07-22 21:21:31 +0200

    autoreview

--- src/utils/Hooks/useFetch.js
+++ src/utils/Hooks/useFetch.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 import { HEADERS } from '../../config/config';
 
+// TODO merge with useDataFetch
 export default function useFetch({ method, url, options }) {
   const [response, setResponse] = useState({});
   const [error, setError] = useState(false);
