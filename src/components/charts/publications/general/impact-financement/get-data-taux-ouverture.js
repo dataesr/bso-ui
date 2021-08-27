e6f22858603dc5fae175d20a67a50a486dbf182f
eric <eric.jeangirard@recherche.gouv.fr>
2021-08-27 18:44:51 +0200

    fix graph grants

--- src/components/charts/publications/general/impact-financement/get-data-taux-ouverture.js
+++ src/components/charts/publications/general/impact-financement/get-data-taux-ouverture.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import Axios from 'axios';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
 
 import { ES_API_URL, HEADERS } from '../../../../../config/config';
@@ -11,125 +11,128 @@ import {
 } from '../../../../../style/colours.module.scss';
 import { getFetchOptions } from '../../../../../utils/helpers';
 
-function useGetData(observationSnap, agency) {
+function useGetData(observationSnap, agency = '*') {
   const intl = useIntl();
   const [allData, setData] = useState({});
-  const [agencies, setAgencies] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
-  const getDataForLastObservationSnap = useCallback(
-    async (lastObservationSnap) => {
-      const queryFilter = [];
-      if (agency) {
-        queryFilter.push({ term: { 'grants.agency.keyword': agency } });
-      }
-      const query = getFetchOptions(
-        'openingRate',
-        'health',
-        lastObservationSnap,
-        queryFilter,
-      );
-      const res = await Axios.post(ES_API_URL, query, HEADERS).catch((e) => console.log(e));
-      const data = res.data.aggregations.by_publication_year.buckets;
+  async function getDataForLastObservationSnap(lastObservationSnap) {
+    const queries = [];
+    const queryFilter = [];
+    queryFilter.push({ wildcard: { 'grants.agency.keyword': agency } });
+    const queryFiltered = getFetchOptions(
+      'openingRate',
+      'health',
+      lastObservationSnap,
+      queryFilter,
+    );
+    const query = getFetchOptions(
+      'openingRate',
+      'health',
+      lastObservationSnap,
+      [],
+    );
+    queries.push(Axios.post(ES_API_URL, queryFiltered, HEADERS));
+    queries.push(Axios.post(ES_API_URL, query, HEADERS));
+    const res = await Axios.all(queries).catch(() => {
+      setLoading(false);
+    });
+    let dataAgency = res[0].data.aggregations.by_publication_year.buckets;
+    let data = res[1].data.aggregations.by_publication_year.buckets;
+    console.log('ttt2', dataAgency);
 
-      // Tri pour avoir les années dans l'ordre d'affichage du graphe
-      data.sort((a, b) => a.key - b.key);
+    // Tri pour avoir les années dans l'ordre d'affichage du graphe
+    data = data.sort((a, b) => a.key - b.key);
+    dataAgency = dataAgency.sort((a, b) => a.key - b.key);
 
-      const categories = []; // Elements d'abscisse
-      const all = [];
-      const withDeclaration = [];
-      const withoutDeclaration = [];
-
-      data
-        .filter(
-          (el) => el.key > 2012
+    const categories = []; // Elements d'abscisse
+    const all = [];
+    const withDeclaration = [];
+    const withoutDeclaration = [];
+    dataAgency
+      .filter(
+        (el) => el.key > 2012
             && parseInt(el.key, 10)
               < parseInt(lastObservationSnap.substring(0, 4), 10),
-        )
-        .forEach((el) => {
-          categories.push(el.key);
+      )
+      .forEach((el) => {
+        // avec declaration
+        const withDeclarationElements = el.by_has_grant.buckets.find(
+          (item) => item.key === 1,
+        );
+        const withDeclarationOa = withDeclarationElements.by_is_oa.buckets.find(
+          (item) => item.key === 1,
+        )?.doc_count || 0;
+        withDeclaration.push({
+          y: (100 * withDeclarationOa) / withDeclarationElements.doc_count,
+          y_abs: withDeclarationOa,
+          y_tot: withDeclarationElements.doc_count,
+          publicationDate: el.key,
+          agency: (agency === '*' ? intl.formatMessage({ id: 'app.all-agencies' }) : agency),
+        });
+      });
 
-          // avec ou sans declaration
-          const Oa = el.by_is_oa.buckets.find((item) => item.key === 1)?.doc_count || 0;
-          all.push({
-            y: (100 * Oa) / el.doc_count,
-            y_abs: Oa,
-            y_tot: el.doc_count,
-            publicationDate: el.key,
-          });
-          // avec declaration
-          const withDeclarationElements = el.by_has_grant.buckets.find(
-            (item) => item.key === 1,
-          );
-          const withDeclarationOa = withDeclarationElements.by_is_oa.buckets.find(
-            (item) => item.key === 1,
-          )?.doc_count || 0;
-          withDeclaration.push({
-            y: (100 * withDeclarationOa) / withDeclarationElements.doc_count,
-            y_abs: withDeclarationOa,
-            y_tot: withDeclarationElements.doc_count,
-            publicationDate: el.key,
-          });
-          // sans declaration
-          const withoutDeclarationElements = el.by_has_grant.buckets.find(
-            (item) => item.key === 0,
-          );
-          const withoutDeclarationOa = withoutDeclarationElements.by_is_oa.buckets.find(
-            (item) => item.key === 1,
-          )?.doc_count || 0;
-          withoutDeclaration.push({
-            y:
+    data
+      .filter(
+        (el) => el.key > 2012
+            && parseInt(el.key, 10)
+              < parseInt(lastObservationSnap.substring(0, 4), 10),
+      )
+      .forEach((el, index) => {
+        categories.push(el.key);
+
+        // avec ou sans declaration
+        const Oa = el.by_is_oa.buckets.find((item) => item.key === 1)?.doc_count || 0;
+        all.push({
+          y: (100 * Oa) / el.doc_count,
+          y_abs: Oa,
+          y_tot: el.doc_count,
+          publicationDate: el.key,
+          agency: intl.formatMessage({ id: 'app.grant.withAndWithout' }),
+        });
+        // avec declaration, on s'assure qu'il y a bien toutes années, sinon on complète par un null
+        if (withDeclaration[index].publicationDate > el.key) {
+          withDeclaration.unshift(null);
+        }
+        // sans declaration
+        const withoutDeclarationElements = el.by_has_grant.buckets.find(
+          (item) => item.key === 0,
+        );
+        const withoutDeclarationOa = withoutDeclarationElements.by_is_oa.buckets.find(
+          (item) => item.key === 1,
+        )?.doc_count || 0;
+        withoutDeclaration.push({
+          y:
               (100 * withoutDeclarationOa)
               / withoutDeclarationElements.doc_count,
-            y_abs: withoutDeclarationOa,
-            y_tot: withoutDeclarationElements.doc_count,
-            publicationDate: el.key,
-          });
-          // si une agence en particulier est sélectionnée
-          // TODO : ajouter une 4e barre
+          y_abs: withoutDeclarationOa,
+          y_tot: withoutDeclarationElements.doc_count,
+          publicationDate: el.key,
+          agency: intl.formatMessage({ id: 'app.no-grant' }),
         });
+        // si une agence en particulier est sélectionnée
+        // TODO : ajouter une 4e barre
+      });
+    const dataGraph = [
+      {
+        name: intl.formatMessage({ id: 'app.all-publications' }),
+        data: all,
+        color: discipline100,
+      },
+      {
+        name: intl.formatMessage({ id: 'app.with-declaration' }),
+        data: withDeclaration,
+        color: discipline150,
+      },
+      {
+        name: intl.formatMessage({ id: 'app.without-declaration' }),
+        data: withoutDeclaration,
+        color: nonconnu,
+      },
+    ];
 
-      const dataGraph = [
-        {
-          name: intl.formatMessage({ id: 'app.all-publications' }),
-          data: all,
-          color: discipline100,
-        },
-        {
-          name: intl.formatMessage({ id: 'app.with-declaration' }),
-          data: withDeclaration,
-          color: discipline150,
-        },
-        {
-          name: intl.formatMessage({ id: 'app.without-declaration' }),
-          data: withoutDeclaration,
-          color: nonconnu,
-        },
-      ];
-
-      return { categories, dataGraph };
-    },
-    [intl, agency],
-  );
-
-  async function GetAllAgencies() {
-    const query = getFetchOptions('allAgencies');
-    const res = await Axios.post(ES_API_URL, query, HEADERS).catch((e) => console.log(e));
-    const data = res.data.aggregations.by_agency.buckets;
-    return data;
+    return { categories, dataGraph };
   }
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const allAgencies = await GetAllAgencies();
-        setAgencies(allAgencies.map((el) => el.key));
-      } catch (error) {
-        setLoading(false);
-      }
-    }
-    getData();
-  }, []);
 
   useEffect(() => {
     async function getData() {
@@ -144,6 +147,6 @@ function useGetData(observationSnap, agency) {
     getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [observationSnap, agency]);
-  return { allData, isLoading, agencies };
+  return { allData, isLoading };
 }
 export default useGetData;
