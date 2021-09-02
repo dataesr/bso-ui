e90f562f1531c07460424d560ca42fb02589e5e3
eric <eric.jeangirard@recherche.gouv.fr>
2021-09-02 16:04:02 +0200

    fix listing editeurs / archives

--- src/components/charts/publications/archives/dynamique-ouverture/chart-taux-ouverture.js
+++ src/components/charts/publications/archives/dynamique-ouverture/chart-taux-ouverture.js
@@ -41,7 +41,7 @@ const Chart = ({ graphFooter, graphComments, id, domain }) => {
   const { dataGraph1 } = data;
   const query = getFetchOptions(
     'repositoriesList',
-    'health',
+    domain,
     lastObservationSnap,
   );
   const term = {};
