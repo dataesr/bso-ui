e3212ea671713478b5bbbe635431ed06787d3edf
PaulineGC <paulinegc@protonmail.com>
2021-09-20 16:14:28 +0200

    remove project and theme (#164)
    * remove project and theme
    * fix essais page load

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import FAQ from './pages/APropos/FAQ';
 import Glossaire from './pages/APropos/Glossaire';
 import Methodologie from './pages/APropos/Methodologie';
 import NotesFlash from './pages/APropos/NotesFlash';
-import Projet from './pages/APropos/Projet';
 import BaroNational from './pages/BaroNational';
 import NationalPublications from './pages/BaroNational/NationalPublications';
 import BaroSante from './pages/BaroSante';
@@ -17,8 +16,6 @@ import EssaisCliniques from './pages/BaroSante/EssaisCliniques';
 import Etudes from './pages/BaroSante/Etudes';
 import SantePublications from './pages/BaroSante/SantePublications';
 import Integration from './pages/Integration';
-import Themes from './pages/Themes';
-import Theme1 from './pages/Themes/Theme-1';
 import messagesEN from './translations/en.json';
 import messagesFR from './translations/fr.json';
 import TranslationPage from './translations/translations-page';
@@ -75,7 +72,9 @@ function App() {
               .map((tab) => Object.keys(tab).map((l) => tab[l]))
               .flat(1)}
           >
-            <EssaisCliniques />
+            <GraphNavigationContextProvider>
+              <EssaisCliniques />
+            </GraphNavigationContextProvider>
           </Route>
 
           <Route
@@ -85,19 +84,6 @@ function App() {
               .flat(1)}
           >
             <Etudes />
-          </Route>
-
-          <Route
-            exact
-            path={Object.keys(urls.themes).map((l) => urls.themes[l])}
-          >
-            <Themes />
-          </Route>
-          <Route
-            exact
-            path={Object.keys(urls.themes1).map((l) => urls.themes1[l])}
-          >
-            <Theme1 />
           </Route>
           <Route
             exact
@@ -115,12 +101,6 @@ function App() {
             path={Object.keys(urls.glossaire).map((l) => urls.glossaire[l])}
           >
             <Glossaire />
-          </Route>
-          <Route
-            exact
-            path={Object.keys(urls.projet).map((l) => urls.projet[l])}
-          >
-            <Projet />
           </Route>
           <Route exact path={Object.keys(urls.flash).map((l) => urls.flash[l])}>
             <NotesFlash />
