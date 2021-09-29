8b5faf80c4480db44d12b2fc543aabbd12f9302b
Jerem <jeremy.peglion@gmail.com>
2021-09-29 16:27:59 +0200

    suppresion du selecteur de theme

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ import App from './App';
 import LANGUAGES from './config/supported-languages';
 import { GlobalsContextProvider } from './utils/Hooks/useGetGlobals';
 import { LangContextProvider } from './utils/Hooks/useLang';
-import { ThemeContextProvider } from './utils/Hooks/useTheme';
 
 ReactDOM.render(
   <LangContextProvider supportedLanguages={LANGUAGES}>
-    <ThemeContextProvider>
-      <GlobalsContextProvider>
-        <App />
-      </GlobalsContextProvider>
-    </ThemeContextProvider>
+    <GlobalsContextProvider>
+      <App />
+    </GlobalsContextProvider>
   </LangContextProvider>,
   document.getElementById('root'),
 );
