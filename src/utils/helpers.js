8ed8be8ec84dea469d255120097ec657de7218dd
eric <eric.jeangirard@recherche.gouv.fr>
2021-10-25 12:38:33 +0200

    more visibible top labels

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -105,6 +105,7 @@ export function getPercentageYAxis(showTotal = true, max = null) {
       },
       style: {
         fontWeight: 'bold',
+        fontSize: '15px',
       },
     },
     labels: {
