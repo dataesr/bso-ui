1645bfe7b12872843a4b44951fb54e05763ad198
Anne L'Hôte <anne.lhote@gmail.com>
2022-01-31 18:45:01 +0100

    fix(graphes): Résoud un warning de dépendance

--- src/components/DataCardsSection/index.js
+++ src/components/DataCardsSection/index.js
@@ -256,6 +256,7 @@ export default function DataCardSection({ domain, lang }) {
       hostedDocumentsPMC,
       lang,
       lastObservationSnap,
+      oaBooksRate,
       openHealthPublicationPublisherRepository,
       openPublicationRate,
       publicationsNumber,
