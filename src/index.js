ee9a875e9253a02b8fc4e9dd5596230755f27615
Anne L'Hôte <anne.lhote@gmail.com>
2022-08-01 11:10:58 +0200

    fix(libs): Upgrade libs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import LANGUAGES from './config/supported-languages';
 import { GlobalsContextProvider } from './utils/Hooks/useGetGlobals';
 import { LangContextProvider } from './utils/Hooks/useLang';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <LangContextProvider supportedLanguages={LANGUAGES}>
     <GlobalsContextProvider>
       <App />
     </GlobalsContextProvider>
   </LangContextProvider>,
-  document.getElementById('root'),
 );
