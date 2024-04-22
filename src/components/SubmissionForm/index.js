9131a87e92fc8be741213b03f48a4aa3b8187062
Anne L'Hôte <anne.lhote@gmail.com>
2024-04-22 10:36:04 +0200

    Revert "fix(submission): log needed to debug"
    This reverts commit 61e0f8fe408500f3bc642ca24c30bd5e07a9222d.

--- src/components/SubmissionForm/index.js
+++ src/components/SubmissionForm/index.js
@@ -81,10 +81,6 @@ const SubmissionForm = () => {
   };
 
   const getPreviousDoiCount = async () => {
-    console.log('getPreviousDoiCount');
-    console.log(REACT_APP_OS_PASSWORD);
-    console.log(REACT_APP_OS_TENANT_NAME);
-    console.log(REACT_APP_OS_USERNAME);
     if (
       REACT_APP_OS_PASSWORD
       && REACT_APP_OS_TENANT_NAME
