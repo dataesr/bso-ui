bb3bff8de4ad534c350d5640f88a5f098c152cdf
eric <eric.jeangirard@recherche.gouv.fr>
2024-10-17 15:09:07 +0200

    bso3 datasets graph

--- src/utils/chartComponents.js
+++ src/utils/chartComponents.js
@@ -290,6 +290,11 @@ const chartComponents = {
       '../components/Charts/data/disciplines/mentions/datasets-with-at-least-one-explicit-mention'
     ),
   ),
+  'data.general.repositories.datasets-by-publisher': lazy(() => import(
+    '../components/Charts/data/general/repositories/datasets-by-publisher'
+  )),
+  'data.general.repositories.datasets-by-size': lazy(() => import('../components/Charts/data/general/repositories/datasets-by-size')),
+  'data.general.repositories.datasets-by-format': lazy(() => import('../components/Charts/data/general/repositories/datasets-by-format')),
   // Orcid
   'orcid.general.chart-evolution': lazy(() => import('../components/Charts/orcid/general/chart-evolution')),
   'orcid.general.creation-by-year': lazy(() => import('../components/Charts/orcid/general/creation-by-year')),
